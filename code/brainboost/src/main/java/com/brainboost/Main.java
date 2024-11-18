97594ddb819d7422946ec756cff9803641a6c71e
Leonardo Cruz <152569224+Choshibear@users.noreply.github.com>
2024-11-17 23:03:11 -0800

    Update Main.java

--- code/brainboost/src/main/java/com/brainboost/Main.java
+++ code/brainboost/src/main/java/com/brainboost/Main.java
@@ -1,16 +1,26 @@
 package com.brainboost;
-import com.brainboost.frames.*;
+import javax.swing.JFrame;
+
+import com.brainboost.frames.LoginFrame;
 
 public class Main 
 {
     public static void main( String[] args ) {
-        // try {
-        //     String response = ServerAPI.sendMessage("Hello from Client!");
-        //     System.out.println("Received from server: " + response);
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }
-        LoginFrame lf = new LoginFrame();
-        lf.setVisible(true);
+        try {
+            String response = ServerAPI.sendMessage("Hello from Client!");
+            System.out.println("Received from server: " + response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            }
+        JFrame frame = new JFrame();
+
+        //global window frame settings
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 600);
+        frame.setTitle("BrainBoost");
+        frame.setLocationRelativeTo(null);
+
+        frame.setContentPane(new LoginFrame(frame));
+        frame.setVisible(true);
     }
 }
