0cb7d86a63eec3ee6823d158661f6b112fb9f751
Leonardo Cruz <152569224+Choshibear@users.noreply.github.com>
2024-11-25 18:00:28 -0800

    Update User.java

--- code/brainboost/src/main/java/com/brainboost/User.java
+++ code/brainboost/src/main/java/com/brainboost/User.java
@@ -2,9 +2,11 @@ package com.brainboost;
 public class User {
     private String username;
     private String password;
+    private int quiz_id;
     public User() {
         username = null;
         password = null;
+        quiz_id = 0;
     }
     public User(String username, String password) {
         this.username = username;
@@ -15,5 +17,17 @@ public class User {
     }
     public String getPassword() {
         return password;
+    }
+    public int getQuiz_id() {
+        return quiz_id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setQuiz_id(int quiz_id) {
+        this.quiz_id = quiz_id;
     }
 }
